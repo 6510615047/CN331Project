432dc141c291662730021d53db9ab21c03152b3f
Jirat Charoenkaew <6510615054@student.tu.ac.th>
2024-11-16 17:14:41 +0700

    test word ok

--- FlashBook/folWordSel/test_views.py
+++ FlashBook/folWordSel/test_views.py
@@ -16,7 +16,6 @@ class FolderTests(TestCase):
         )
 
         self.client = Client()
-        # self.user.post(reverse("login"), {"user": self.user.user, "password": self.user.password})
 
         # Create initial folders and words
         self.folder1 = Folder.objects.create(user=self.user, folder_name='Folder1')
@@ -47,12 +46,21 @@ class FolderTests(TestCase):
             'action': 'edit',
             'folder_name': 'UpdatedFolder'
         })
-        self.assertEqual(Folder.objects.get(folder_id=self.folder1.folder_id).folder_name, 'UpdatedFolder')
+        self.assertEqual(Folder.objects.get(user=self.user,folder_id=self.folder1.folder_id).folder_name, 'UpdatedFolder')
+    
+    def test_edit_duplicate_name_folder(self):
+        """Test editing an existing folder."""
+        response = self.client.post(reverse('edit_folder', args=[self.folder1.folder_id]), {
+            'action': 'edit',
+            'folder_name': 'Folder2'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('folder'))
 
     def test_delete_folder(self):
         """Test deleting an existing folder."""
         response = self.client.post(reverse('edit_folder', args=[self.folder1.folder_id]), {'action': 'delete'})
-        self.assertFalse(Folder.objects.filter(folder_id=self.folder1.folder_id).exists())
+        self.assertFalse(Folder.objects.filter(user=self.user,folder_id=self.folder1.folder_id).exists())
 
     def test_folder_search_success(self):
         """Test searching for a folder."""
@@ -72,5 +80,89 @@ class FolderTests(TestCase):
         self.assertContains(response, 'Folder1')
         self.assertContains(response, 'Folder2')
 
+class WordTests(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create(
+            user_id=1, 
+            user='testuser',
+            fname='testfname',
+            lname='testlname', 
+            password='testpassword',
+            email='testemail@test.com'
+        )
 
+        self.client = Client()
 
+        # Create initial folders and words
+        self.folder = Folder.objects.create(user=self.user, folder_name='Folder')
+
+        self.word1 = Word.objects.create(user=self.user, folder=self.folder,word='testword1',meaning='testmeaning1')
+        self.word2 = Word.objects.create(user=self.user, folder=self.folder,word='testword2',meaning='testmeaning2')
+
+    def test_word_view(self):
+        response = self.client.get(reverse('word', args=[self.folder.folder_id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'testword1')
+        self.assertContains(response, 'testword2')
+        self.assertTemplateUsed(response, 'word.html')
+
+    def test_add_word(self):
+        response = self.client.post(reverse('add_word', args=[self.folder.folder_id]), {
+            'action': 'add',
+            'word_name': 'NewWord',
+            'meaning': 'NewMeaning'
+        })
+        self.assertTrue(Word.objects.filter(user=self.user,folder=self.folder, word='NewWord').exists())
+
+    def test_add_duplicate_word(self):
+        response = self.client.post(reverse('add_word', args=[self.folder.folder_id]), {
+            'action': 'add',
+            'word_name': 'testword1',
+            'meaning': 'testmeaning1'
+        })
+        messages = list(response.wsgi_request._messages)
+        self.assertTrue(any('Word with this name already exists.' in str(m) for m in messages))
+
+    def test_edit_word(self):
+        response = self.client.post(reverse('edit_word', args=[self.folder.folder_id, self.word1.word_id]), {
+            'action': 'edit',
+            'word_name': 'testword3',
+            'meaning': 'testmeaning3'
+        })
+        word = Word.objects.get(word_id=self.word1.word_id)
+        self.assertEqual(word.word, 'testword3')
+        self.assertEqual(word.meaning, 'testmeaning3')
+
+    def test_edit_duplicate_name_word(self):
+        response = self.client.post(reverse('edit_word', args=[self.folder.folder_id, self.word1.word_id]), {
+            'action': 'edit',
+            'word_name': 'testword2',
+            'meaning': 'testmeaning2'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_word(self):
+        """Test deleting a word in a folder."""
+        response = self.client.post(reverse('edit_word', args=[self.folder.folder_id, self.word1.word_id]), {
+            'action': 'delete'
+        })
+        self.assertFalse(Word.objects.filter(word_id=self.word1.word_id).exists())
+
+    def test_search_word_success(self):
+        """Test searching for a word."""
+        response = self.client.get(reverse('search_word', args=[self.folder.folder_id]) + '?query=testword1')
+        self.assertContains(response, 'testword1')
+        self.assertNotContains(response, 'testword2')
+
+    def test_search_word_failure(self):
+        """Test searching for a word."""
+        response = self.client.get(reverse('search_word', args=[self.folder.folder_id]) + '?query=testword3')
+        self.assertNotContains(response, 'testword1')
+        self.assertNotContains(response, 'testword2')
+
+    def test_search_word_empty(self):
+        """Test searching for a word."""
+        response = self.client.get(reverse('search_word', args=[self.folder.folder_id]) + '?query=')
+        self.assertContains(response, 'testword1')
+        self.assertContains(response, 'testword2')
