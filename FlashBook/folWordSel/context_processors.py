a440e4ead44106e9be311ad1ff9321033acf311c
Jirat Charoenkaew <6510615054@student.tu.ac.th>
2024-11-22 21:03:37 +0700

    check-in v3 disable button when checked-in

---
+++ FlashBook/folWordSel/context_processors.py
@@ -0,0 +1,13 @@
+from datetime import date
+from homepage.models import User
+
+def check_in_today(request):
+    # ตรวจสอบว่าผู้ใช้ล็อกอินหรือยัง
+    if request.session.get('user_id'):
+        user = User.objects.get(user_id=request.session.get('user_id'))
+        today = date.today()
+
+        # ตรวจสอบว่า user ได้เช็คอินในวันนี้หรือไม่
+        is_checked_in_today = user.last_check_in == today
+        return {'is_checked_in_today': is_checked_in_today}
+    return {'is_checked_in_today': False}
