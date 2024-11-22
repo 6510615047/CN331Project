30e10f855f15b87417e22a7cc52dc521de51d5ab
Jirat Charoenkaew <6510615054@student.tu.ac.th>
2024-11-23 00:36:35 +0700

    color user!

--- FlashBook/folWordSel/context_processors.py
+++ FlashBook/folWordSel/context_processors.py
@@ -6,9 +6,8 @@ def check_in_today(request):
     if request.session.get('user_id'):
         user = User.objects.get(user_id=request.session.get('user_id'))
         today = date.today()
-        print(today)
-        print(user.last_check_in)
         # ตรวจสอบว่า user ได้เช็คอินในวันนี้หรือไม่
         is_checked_in_today = user.last_check_in == today
-        return {'is_checked_in_today': is_checked_in_today,'user': user}
-    return {'is_checked_in_today': False,'user': user}
+        card_color_is_hash = user.card_color.startswith('#') if user.card_color else False
+        return {'is_checked_in_today': is_checked_in_today,'user': user, 'card_color_is_hash': card_color_is_hash}
+    return {'is_checked_in_today': False}
